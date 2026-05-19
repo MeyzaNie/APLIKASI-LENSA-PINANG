12c01d576f8503b07561062fa3c9829b5ecb75c5
MeyzaNie <mey91986@gmail.com>
2026-05-19 07:22:54 +0700

    Update beranda_admin.dart

--- lib/screens/beranda_admin.dart
+++ lib/screens/beranda_admin.dart
@@ -10,7 +10,8 @@ class BerandaAdmin extends StatefulWidget {
 }
 
 class _BerandaAdminState extends State<BerandaAdmin> {
-  int _index = 0;
+  int _index = 2; // Default langsung aktif ke halaman Verifikasi sesuai gambar
+
   final List<Widget> _pages = [
     KelolaPengguna(),
     KelolaDestinasiAdmin(),
@@ -21,16 +22,44 @@ class _BerandaAdminState extends State<BerandaAdmin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // APPBAR KOTAK PUTIH ATAS DIHAPUS DARI SINI AGAR TIDAK TUMPANG TINDIH
       body: _pages[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
-        onTap: (i) => setState(() => _index = i),
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Pengguna'),
-          BottomNavigationBarItem(icon: Icon(Icons.place), label: 'Destinasi'),
-          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Pemesanan'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
-        ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey.shade200, width: 1)),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _index,
+          onTap: (i) => setState(() => _index = i),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: const Color(0xFF4A709C),
+          unselectedItemColor: Colors.grey.shade400,
+          selectedLabelStyle: const TextStyle(fontSize: 11, fontWeight: FontWeight.w600),
+          unselectedLabelStyle: const TextStyle(fontSize: 11),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.group_outlined),
+              activeIcon: Icon(Icons.group),
+              label: 'Pengguna',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map_outlined),
+              activeIcon: Icon(Icons.map),
+              label: 'Destinasi',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.qr_code_scanner),
+              label: 'Verifikasi',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
+              label: 'Profil',
+            ),
+          ],
+        ),
       ),
     );
   }
